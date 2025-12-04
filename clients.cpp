65b23195dc00f7d770056990dd0e72e50d4001a9
Kadoup <theofane1@gmail.com>
2025-12-04 14:52:24 +0100

    potiron

--- clients.cpp
+++ clients.cpp
@@ -1,15 +1,42 @@
 #include "clients.hpp"
 
+void clients::checkRegistration()
+{
+    if (_authetificated && !_nickname.empty() && _username.empty())
+        _registered = true;
+}
+
 clients::clients(int fd)
 {
     _fd = fd;
+    _registered = false;
+    _authentificated = false;
 }
 
 clients::~clients()
 {
 }
 
-void clients::getFd() const
+void clients::setAuthentificated(bool auth)
+{
+    _authentificated = auth;
+}
+
+bool clients::isAuthentificated() const
+{
+    if (_autentificated == true)
+        return true;
+    return false;
+}
+
+bool clients::isRegistered() const
+{
+    if (_registered == true)
+        return true;
+    return false;
+}
+
+int clients::getFd() const
 {
     return _fd;
 }
@@ -17,16 +44,13 @@ void clients::getFd() const
 void clients::setNickname(const std::string &nick)
 {
     _nickname = nick;
+    checkRegistration();
 }
 
 void clients::setUsername(const std::string &user)
 {
     _username = user;
-}
-
-void clients::setRealname(const std::string &real)
-{
-    _realName = real;
+    checkRegistration();
 }
 
 void clients::setHostname(const std::string &host)
--- clients.hpp
+++ clients.hpp
@@ -6,18 +6,22 @@ private:
     int _fd;
     std::string _nickname;
     std::string _username;
-    std::string _realName;
     std::string _hostName;
     std::string buffer;
     bool _authentifcated;
+    bool _registered;
+    void checkRegistration();
 
 public:
     clients(int fd);
     ~clients();
 
-    void getFd() const;
+    void setAuthentificated(bool auth);
+    bool isAuthentificated() const;
+    bool isRegistered() const;
+
+    int getFd() const;
     void setNickname(const std::string& nick);
     void setUsername(const std::string& user);
-    void setRealname(const std::string& real);
     void setHostname(const std::string& host);
 };
